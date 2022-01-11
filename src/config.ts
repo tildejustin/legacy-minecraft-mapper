7bcf2790dc1795063bf3d6017e230fd6f20a0ea4
logwet <logwet@users.noreply.github.com>
2022-01-11 12:59:59 +1100

    Change API endpoints to legacyfabric

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,4 @@
 
-export const YARN_VERSION_ENDPOINT = "https://meta.fabricmc.net/v2/versions/yarn";
+export const YARN_VERSION_ENDPOINT = "https://meta.legacyfabric.net/v2/versions/yarn";
 
-export const YARN_JAR_URL = "https://maven.fabricmc.net/net/fabricmc/yarn/";
+export const YARN_JAR_URL = "https://maven.legacyfabric.net/net/fabricmc/yarn/";
